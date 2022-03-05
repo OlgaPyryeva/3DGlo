9fe5d39038d5109a1d1252254689edcd7816e509
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-05 14:18:11 +0300

    add(modal.js): add func animate for modal window

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,9 +1,9 @@
+import { animate } from "./helpers";
+
 const modal = () => {
   const modal = document.querySelector(".popup");
   const modalForm = modal.querySelector(".popup-content");
   const buttons = document.querySelectorAll(".popup-btn");
-
-  console.log(modalForm);
 
   let width = document.documentElement.clientWidth;
 
@@ -16,54 +16,48 @@ const modal = () => {
       !e.target.closest(".popup-content") ||
       e.target.classList.contains("popup-close")
     ) {
-      animationModal(false);
-      // modal.style.display = "none";
+      if (width >= 768) {
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modalForm.style.opacity = 1 - progress;
+            modalForm.style.top = 10 - progress * 10 + "%";
+            if (modalForm.style.opacity < 0.1) {
+              modal.style.display = "none";
+            }
+          },
+        });
+      } else {
+        modal.style.display = "none";
+      }
     }
   });
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       if (width >= 768) {
-        modalForm.style.opacity = 0;
-        modalForm.style.top = "";
         modal.style.display = "block";
-        animationModal(true);
+
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modalForm.style.top = progress * 10 + "%";
+            modalForm.style.opacity = progress;
+          },
+        });
       } else {
         modal.style.display = "block";
+        modalForm.style.top = 10 + "%";
+        modalForm.style.opacity = 1;
       }
     });
   });
-
-  let animationModal = (showType) => {
-    let start = Date.now(); // запомнить время начала
-    let timer = setInterval(function () {
-      let timePassed = Date.now() - start;
-
-      if (timePassed >= 500) {
-        clearInterval(timer);
-        return;
-      }
-      if (showType) {
-        showAnimation(timePassed);
-        showModal(timePassed);
-      } else {
-        hideModal(timePassed);
-      }
-    }, 20);
-
-    function showAnimation(timePassed) {
-      modalForm.style.top = timePassed / 50 + "%";
-    }
-    function showModal(timePassed) {
-      modalForm.style.opacity = timePassed / 500;
-    }
-    function hideModal(timePassed) {
-      modalForm.style.opacity = 1 - timePassed / 500;
-      if (modalForm.style.opacity < 0.1) {
-        modal.style.display = "none";
-      }
-    }
-  };
 };
 
 export default modal;
